fd949050b9c85bcd0e7332dcd73c3cafea478c65
Tom Sedlmeier <TomSedlmeier@hotmail.de>
2026-07-27 08:55:30 +0200

    Support green speed measurement identifiers

--- GreenSpeedMes.cs
+++ GreenSpeedMes.cs
@@ -4,10 +4,14 @@ public partial class GreenSpeedMes
 {
     [Key]
     public int GreenSpeedMesId { get; set; }
+
+    [Required, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid PubId { get; set; }
+
     public int GreenSpeedId { get; set; }
     public DateTime MesTime { get; set; }
     public decimal MesSpeed { get; set; }
     public virtual int AreaId => GreenSpeed.AreaId;
 
     public required GreenSpeed GreenSpeed { get; set; }
-}
+}
